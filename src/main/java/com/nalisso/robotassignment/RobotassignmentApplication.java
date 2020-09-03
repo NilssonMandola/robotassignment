4cd07a3b73668f80dcaa73f2588768493ac99357
Ola Nilsson <olanilsson95@gmail.com>
2020-09-03 17:55:16 +0200

    init commit

---
+++ src/main/java/com/nalisso/robotassignment/RobotassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nalisso.robotassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotassignmentApplication.class, args);
+	}
+
+}
